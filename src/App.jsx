e2b71b6695a4c4a2831c3b279d6bc90911a8c6ed
kevincisnero01 <kevingabriel18@gmail.com>
2025-10-07 21:13:29 -0400

    crear compomente header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <h1>React Basico</h1>
+      <Header/>
+      
+      <Footer/>
     </>
   )
 }
