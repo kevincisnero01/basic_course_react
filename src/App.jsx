e3ebe7423db4a2b523aaa2d163994ccf62a5fa7e
kevincisnero01 <kevingabriel18@gmail.com>
2025-10-11 22:52:56 -0400

    primeros pasos xon useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  //Crear lista de productos
+  const [products, setProduct] = useState([
+      {id: 1, name: "Harina Pan", price: 200},
+      {id: 2, name: "Arroz", price: 300},
+      {id: 3, name: "Pasta", price: 400}
+    ]);
+  
+  //Obtener fecha 
   const currentYear = new Date().getFullYear();
   return (
     <>
