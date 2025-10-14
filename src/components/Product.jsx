2e80e1b88dc618d340f645d22f3fd340c3555240
Kevin Cisnero <kevingabriel18@gmail.com>
2025-10-13 22:42:56 -0400

    eventos en react

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,9 +2,26 @@ import React from "react";
 
 function Product({product}){
   
-  const {name, price} = product;
+  //destructurar propiedades
+  const {id, name, price} = product;
+
+  //Agregar producto al carrito
+  const  selectProducts  = (id) =>{
+    console.log('Producto:'+id);
+  }
+
   return(
-    <div>{name} = {price}</div>
+    <div>
+      <p>Proucto: {name}</p> 
+      <p>Precio: {price}$</p> 
+      <button
+        type="button" 
+        style={{ padding: '2px 4px', border: '1px solid gray' }} 
+        onClick={ () => selectProducts(id) }
+      >
+        Comprar
+      </button>
+    </div>
     )
 }
 
